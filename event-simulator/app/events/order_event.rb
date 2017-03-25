fc19bc576cd797e92c6747ccc7edf871727da004
andri setiawan <andrisetiawan@me.com>
2017-03-25 13:51:10 +0700

    event-simulator: order & driver event sample

--- event-simulator/app/events/order_event.rb
+++ event-simulator/app/events/order_event.rb
@@ -1,16 +1,32 @@
 class OrderEvent
-  def order_picked_up
+  def gofood_order_booked
     {
       header: {
-        event_name: 'order.picked_up',
+        event_name: 'gofood.order.booked',
         timestamp: '2017-02-03T10:17:40.392Z'
       },
       body: {
-        lat: '171.09',
-        lang: '12.142',
+        location: [-6.178005,106.7881563],
         order_id: 'order-123',
         driver_id: 'D12323',
-        customer_id: 'C1234123'
+        customer_id: 'C1234123',
+        merchant_id: 'M0001'
+      }
+    }
+  end
+
+  def gofood_order_canceled
+    {
+      header: {
+        event_name: 'gofood.order.canceled',
+        timestamp: '2017-02-03T10:17:40.392Z'
+      },
+      body: {
+        location: [-6.178005,106.7881563],
+        order_id: 'order-123',
+        customer_id: 'C1234123',
+        merchant_id: 'M0001',
+        reason: "waited.too.long"
       }
     }
   end
