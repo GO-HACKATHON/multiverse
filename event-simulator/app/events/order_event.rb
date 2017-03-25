2d04f5f848e69f893643c28c52f25a83803ddff1
andri setiawan <andrisetiawan@me.com>
2017-03-25 21:20:10 +0700

    event-simulator: update order & driver event

--- event-simulator/app/events/order_event.rb
+++ event-simulator/app/events/order_event.rb
@@ -3,14 +3,14 @@ class OrderEvent
     {
       header: {
         event_name: 'gofood.order.booked',
-        timestamp: '2017-02-03T10:17:40.392Z'
+        timestamp: current_timestamp
       },
       body: {
         location: Location.generate,
-        order_id: 'order-123',
-        driver_id: 'D12323',
-        customer_id: 'C1234123',
-        merchant_id: 'M0001'
+        order_id: sample_order_id,
+        driver_id: sample_driver_id,
+        customer_id: sample_customer_id,
+        merchant_id: sample_merchant_id
       }
     }
   end
@@ -19,15 +19,32 @@ class OrderEvent
     {
       header: {
         event_name: 'gofood.order.canceled',
-        timestamp: '2017-02-03T10:17:40.392Z'
+        timestamp: current_timestamp
       },
       body: {
         location: Location.generate,
-        order_id: 'order-123',
-        customer_id: 'C1234123',
-        merchant_id: 'M0001',
+        order_id: sample_order_id,
+        customer_id: sample_customer_id,
+        merchant_id: sample_merchant_id,
         reason: "waited.too.long"
       }
     }
+  end
+
+  private
+  def sample_driver_id
+    "driver-#{rand(1000...2000)}"
+  end
+
+  def sample_order_id
+    "order-#{SecureRandom.hex(10)}"
+  end
+
+  def sample_customer_id
+    "customer-#{rand(1000...2000)}"
+  end
+
+  def sample_merchant_id
+    "merchant-#{rand(10...20)}"
   end
 end
