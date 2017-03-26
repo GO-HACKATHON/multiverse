c5bd646c8fe707a75318d3549cc21d7d03e2b61e
andri setiawan <andrisetiawan@me.com>
2017-03-26 07:02:44 +0700

    add daily stats

--- stremer/lib/stremer.rb
+++ stremer/lib/stremer.rb
@@ -123,6 +123,17 @@ module Stremer
       @consumer.subscribe("event")
     end
 
+    def init_daily_bins_for_key key, client
+      bin_map = {
+        '01' => [], '02'=> [], '03' => [], '04'=> [], '05' => [], '06'=> [],
+        '07'=> [], '08' => [], '09'=> [], '10' => [], '11'=> [], '12' => [], '13'=> [],
+        '14'=> [], '15' => [], '16'=> [], '17' => [], '18'=> [], '19' => [], '20'=> [],
+        '21'=> [], '22' => [], '23'=> [], '24' => [], '25' => [], '26' => [], '27' => [],
+        '28' => [], '29' => [], '30' => [], '31' => []
+      }
+      client.put(key, bin_map)
+    end
+
     def init_hourly_bins_for_key key, client
       bin_map = {
         '00'=> [], '01' => [], '02'=> [], '03' => [], '04'=> [], '05' => [], '06'=> [],
@@ -155,8 +166,12 @@ module Stremer
           # event_name = "driver.idle.on.location"
           event_name = parsed_message["header"]["event_name"]
           timestamp = parsed_message["header"]["timestamp"].to_i
+
+          day_key = daily_key_for_timestamp(timestamp, event_name)
           hour_key = hourly_key_for_timestamp(timestamp, event_name)
           minute_key = minutely_key_for_timestamp(timestamp, event_name)
+
+          add_point_to_daily_stats(timestamp, client, day_key, point)
           add_point_to_hourly_stats(timestamp, client, hour_key, point)
           add_point_to_minutely_stats(timestamp, client, minute_key, point)
           print(".")
@@ -165,6 +180,16 @@ module Stremer
           puts e.inspect
         end
       end
+    end
+
+    def add_point_to_daily_stats(timestamp, client, key, point)
+      init_daily_bins_for_key(key, client) unless client.exists(key)
+      record = client.get(key)
+      bins_key = Time.at(timestamp).to_datetime.strftime("%d")
+      bin_temp = record.bins[bins_key]
+      bin_temp << point
+      record.bins[bins_key] = bin_temp
+      client.put(key, record.bins)
     end
 
     def add_point_to_hourly_stats(timestamp, client, key, point)
@@ -187,13 +212,18 @@ module Stremer
       client.put(key, record.bins)
     end
 
+    def daily_key_for_timestamp(timestamp, event)
+      pk = Time.at(timestamp).to_datetime.strftime("daily#{event}:%Y%m%d")
+      Aerospike::Key.new("test", event, pk)
+    end
+
     def hourly_key_for_timestamp(timestamp, event)
       pk = Time.at(timestamp).to_datetime.strftime("hourly#{event}:%Y%m%d")
       Aerospike::Key.new("test", event, pk)
     end
 
     def minutely_key_for_timestamp(timestamp, event)
-      pk = Time.at(timestamp).to_datetime.strftime("hourly#{event}:%Y%m%d%H")
+      pk = Time.at(timestamp).to_datetime.strftime("minutely#{event}:%Y%m%d%H")
       Aerospike::Key.new("test", event, pk)
     end
   end
