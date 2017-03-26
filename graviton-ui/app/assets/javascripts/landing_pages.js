d4fa3078e752ed158177849ff295438d053fd6fe
Fauzan Qadri <fauzan.qadri@midtrans.com>
2017-03-26 09:16:34 +0700

    add some filtering from ui

--- graviton-ui/app/assets/javascripts/landing_pages.js
+++ graviton-ui/app/assets/javascripts/landing_pages.js
@@ -9,16 +9,21 @@ $(document).ready(function () {
     renderMap(data);
   });
   
+  $("#eventName").on("change", function (e) {
+    var data = $("#eventName option:selected").val();
+    renderMap(data);
+  });
+  
 });
 
-function renderMap(timeRange = "minutly") {
+function renderMap(timeRange = "minutly", eventName = "order.canceled") {
   window.gMap = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: {lat: -6.27651914761269, lng: 106.7233956751221},
     mapTypeId: 'roadmap'
   });
   
-  $.get("/api/v1/maps?time_range="+timeRange, function (data) {
+  $.get("/api/v1/maps?time_range="+timeRange+"&event_name="+eventName, function (data) {
     var heatPoints = data.map( function (point) {
       return new google.maps.LatLng(point.coordinates[0], point.coordinates[1])
     });
--- graviton-ui/app/controllers/api/v1/maps_controller.rb
+++ graviton-ui/app/controllers/api/v1/maps_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::MapsController < ApplicationController
   
   def index
-    Rails.logger.info params[:time_range]
-    render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    time_range =  params[:time_range]
+    event = params[:event_name]
+    case event
+    when "order.canceled"
+      render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    when "gofood.order.canceled"
+      render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    when "gofood.order.booked"
+      render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    when "driver.active.on.location"
+      render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    when "driver.idle.on.location"
+      render json: CancelationHeatMap.get_data({"time_range" => params[:time_range]})
+    end
   end
 end
